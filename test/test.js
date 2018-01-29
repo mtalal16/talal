3335cdc336eac415717fcd49be296e63ff6cba8a
Muhammad Talal <mtalal16@yahoo.com>
2018-01-29 15:46:30 +0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var myFile = require('../lib/my-file'); // the file you're testing
+var myFile = function test(){}; // the file you're testing
 describe('myFile', function() {
   it('should be a function', function() {
     expect(myFile).to.be.a('function');
